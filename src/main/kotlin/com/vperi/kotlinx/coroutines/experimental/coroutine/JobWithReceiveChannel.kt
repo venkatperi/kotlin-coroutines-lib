6be85d649cbf83a4a083e4514ec12bbae493e1f6
Venkat Peri <venkatperi@gmail.com>
2018-04-08 17:07:07 -0400

    updates

--- src/main/kotlin/com/vperi/kotlinx/coroutines/experimental/coroutine/JobWithReceiveChannel.kt
+++ src/main/kotlin/com/vperi/kotlinx/coroutines/experimental/coroutine/JobWithReceiveChannel.kt
@@ -5,6 +5,12 @@ import kotlinx.coroutines.experimental.AbstractCoroutine
 import kotlinx.coroutines.experimental.channels.ReceiveChannel
 import kotlin.coroutines.experimental.CoroutineContext
 
+/**
+ * Exposes a coroutine that has a outgoing [Channel] as a
+ * [Job] and associated [ReceiveChannel].
+ *
+ * Useful for terminating pipes with [drainAndJoin]
+ */
 abstract class JobWithReceiveChannel<out E>(
   parentContext: CoroutineContext,
   active: Boolean,
@@ -16,6 +22,9 @@ abstract class JobWithReceiveChannel<out E>(
     return receiver.cancel(cause)
   }
 
+  /**
+   * Invokes [drain] followed by [join] to terminate pipes.
+   */
   suspend fun drainAndJoin() {
     receiver.drain()
     join()
